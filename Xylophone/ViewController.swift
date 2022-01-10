6557d8284b454b12acfa80ce898db879da933961
Sancara <santiagocaraballo@gmail.com>
2022-01-09 21:51:44 -0300

    xylophone working

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,18 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("I am pressed")
+        
+        let nota  = sender.titleLabel?.text
+        
+        playSound(note: nota!)
     }
     
-    
+    func playSound(note: String) {
+        guard let url = Bundle.main.url(forResource: "\(note)", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 
 }
 
