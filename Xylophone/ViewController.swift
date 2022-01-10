6dd3d5c8267f8796131cfe449c0c9179f9dd32bd
Sancara <santiagocaraballo@gmail.com>
2022-01-09 22:40:49 -0300

    adding fading effect when pressing

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -20,8 +20,16 @@ class ViewController: UIViewController {
     @IBAction func keyPressed(_ sender: UIButton) {
         
         let nota  = sender.titleLabel?.text
+        sender.alpha = 0.5
         
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    //Bring's sender's opacity back up to fully opaque.
+                    sender.alpha = 1.0
+                }
+
         playSound(note: nota!)
+        
     }
     
     func playSound(note: String) {
